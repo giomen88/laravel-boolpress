f523f4d1f0cc6d227ba88cfd0d8cf4a0d52a80fd
giomen88 <giorgia9288@virgilio.it>
2022-10-07 09:16:08 +0200

    modifica immagine preview del form

--- resources/views/includes/admin/form.blade.php
+++ resources/views/includes/admin/form.blade.php
@@ -32,6 +32,6 @@
                 <label for="image" class="form-label">Immagine</label>
                 <input type="file" id="image" name="image">
             </div>
-            <img src=" {{ $post->image ?? 'https://usbforwindows.com/storage/img/images_3/function_set_default_image_when_image_not_present.png' }} " alt="post image preview" class="img-fluid" id="preview">
+            <img src=" {{ $post->image ? asset('storage/'.$post->image) : 'placeholder' }} " alt="{{ $post->image ? $post->slug : 'https://usbforwindows.com/storage/img/images_3/function_set_default_image_when_image_not_present.png' }}" class="img-fluid" id="preview">
         </div>
 
